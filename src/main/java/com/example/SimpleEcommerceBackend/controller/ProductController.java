2244a1e7ed588e3572891ca798258185d6df9ace
adityatulsyan03 <tulsyanaditya03@gmail.com>
2025-04-10 03:55:01 +0530

    Feat - Basic Api Calling

--- src/main/java/com/example/SimpleEcommerceBackend/controller/ProductController.java
+++ src/main/java/com/example/SimpleEcommerceBackend/controller/ProductController.java
@@ -1,11 +1,9 @@
 package com.example.SimpleEcommerceBackend.controller;
 
-
 import com.example.SimpleEcommerceBackend.model.Product;
 import com.example.SimpleEcommerceBackend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,9 +13,38 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @RequestMapping("/products")
-    public List<Product> getProduct(){
+    @GetMapping("/products")
+    public List<Product> getProduct() {
         return productService.getAllProducts();
     }
 
+    @GetMapping("/products/{id}")
+    public Product getProductById(
+            @PathVariable int id
+    ) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(
+            @RequestBody Product product
+    ) {
+        System.out.println(product);
+        productService.AddProduct(product);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct(
+            @PathVariable int id
+    ) {
+        System.out.println("Product with id: "+id+" deleted.");
+        productService.DeleteProduct(id);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(
+            @RequestBody Product product
+    ){
+        productService.UpdateProduct(product);
+    }
 }
--- src/main/java/com/example/SimpleEcommerceBackend/service/ProductService.java
+++ src/main/java/com/example/SimpleEcommerceBackend/service/ProductService.java
@@ -3,19 +3,43 @@ package com.example.SimpleEcommerceBackend.service;
 import com.example.SimpleEcommerceBackend.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101, "Pen", 10),
             new Product(102, "Box", 50)
-    );
+    ));
 
     public List<Product> getAllProducts() {
         return products;
+    }
+
+    public Product getProductById(int id) {
+        return products.stream()
+                .filter(p-> p.getProductId()==id)
+                .findFirst()
+                .orElse(new Product(0,"No Product Found",0));
+    }
+
+    public void AddProduct(Product product) {
+        products.add(product);
+    }
+
+    public void DeleteProduct(int id) {
+        products.removeIf(p -> p.getProductId()==id);
+    }
+
+    public void UpdateProduct(Product product) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getProductId()==product.getProductId()) {
+                products.set(i, product);
+            }
+        }
     }
 
 }
